f8549a1aec9203918345c21f6352bfd54a01829a
Khoi Nguyen <magnumoptics@gmail.com>
2022-07-25 17:43:42 -0700

    Add more stuffs for the login page

--- nut_app_andrew/lib/login.dart
+++ nut_app_andrew/lib/login.dart
@@ -1,43 +1,156 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'color.dart';
+
+class BackLayer extends StatelessWidget {
+  BackLayer({Key? key}) : super(key: key);
+
+  final Widget svg_google = SvgPicture.asset(
+    '/btn_google.svg',
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.only(top: 2),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                '/gym_weight.png',
+                width: 234,
+                height: 217,
+              ),
+              const SizedBox(
+                height: 86,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('Register',
+                    style: Theme.of(context).textTheme.bodyMedium),
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                      indent: 11,
+                      endIndent: 8.5,
+                    ),
+                  ),
+                  Text(
+                    'or login with',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  const Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                      indent: 8.5,
+                      endIndent: 11,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 44,
+              ),
+              Column(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        svg_google,
+                        Text(
+                          'Google',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              // const SizedBox(
+              //   height: 30,
+              // ),
+              TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Image.asset(
+                      '/btn_facebook.png',
+                      width: 32,
+                      height: 32,
+                    ),
+                    Text(
+                      'Facebook',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
 
 class FrontLayer extends StatelessWidget {
   const FrontLayer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    print(Theme.of(context).colorScheme.background.toString());
-    return Theme(
-      data: Theme.of(context)
-          .copyWith(scaffoldBackgroundColor: kNutBackgroundBlue),
-      child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 70),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/newnut.png',
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 70),
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    '/newnut.png',
                     width: 563,
                     height: 317,
                   ),
-                  const SizedBox(
-                    height: 80.0,
-                  ),
-                  Text(
+                ),
+                const SizedBox(
+                  height: 80.0,
+                ),
+                Expanded(
+                  child: Text(
                     'Nut',
                     style: GoogleFonts.russoOne(
                       textStyle: Theme.of(context).textTheme.titleLarge,
                       fontWeight: FontWeight.w400,
                       fontSize: 144,
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
--- nut_app_andrew/lib/main.dart
+++ nut_app_andrew/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'login.dart';
 import 'color.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nut App',
       theme: _kNutTheme,
-      home: const FrontLayer(),
+      home: BackLayer(),
     );
   }
 }
@@ -33,5 +34,8 @@ ThemeData _buildNutTheme() {
 }
 
 TextTheme _buildNutTextTheme(TextTheme base) {
-  return base.copyWith(titleLarge: base.titleLarge!.copyWith(color: kLogoText));
+  return base.copyWith(
+    titleLarge: base.titleLarge!.copyWith(color: kLogoText),
+    bodyMedium: GoogleFonts.roboto(),
+  );
 }
